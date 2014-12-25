280f09a474f0a49fe016d944ac5288cb0e4b384d
Shubham Gupta <shubham.gupta3112@gmail.com>
2014-12-24 18:15:21 -0800

    v0.0

--- src/Columbus.jl
+++ src/Columbus.jl
@@ -1,5 +1,33 @@
 module Columbus
 
-# package code goes here
+	# package code goes here
+	include("julia_lr.jl")
+	include("julia_ls.jl")
+	include("julia_perceptron.jl")
+	include("julia_svm.jl")
+	include("julia_admm.jl")
+	include("sampling.jl")
+	include("transform.jl")
+	include("stepadd_multicore.jl")
+	include("stepdrop_multicore.jl")
+	include("stepdrop.jl")
+
+	export 
+	least_square, 
+	logit_reg, 
+	perceptron, 
+	svm, 
+	admm_qr, 
+	admm_noqr, 
+	coreset, 
+	naive, 
+	drop_feature, 
+	drop_feature_multicore, 
+	add_feature, 
+	join, 
+	select, 
+	remove
+
+
 
 end # module
